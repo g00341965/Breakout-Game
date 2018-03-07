b3bdd3d31f03812a756e0f8bbe911af2e6eee5be
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-03-07 13:30:58 +0000

    Implementing Game Over

--- js/breakout.js
+++ js/breakout.js
@@ -1,5 +1,3 @@
-
-
 //Setup the canvas 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d")
@@ -16,6 +14,13 @@ var ballRadius = 10;
 
 var ballColour = "#0095DD"
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+var rightPressed = false;
+var leftPressed = false;
+ 
 //Draw the ball 
 function drawBall() {
 	ctx.beginPath();
@@ -25,28 +30,65 @@ function drawBall() {
 	ctx.closePath();
 	
 }
-
-
-
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawPaddle();
 	drawBall();
+	
 	x += dx;
 	y += dy;
 
-	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+	//Bounce the ball off three walls - if it drops off the bottom - Game over!
+	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
 		dy = -dy;
 		ballColour = "red"
 	}
-
-	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
 		dx = -dx;
 		ballColour = "green"
-		
+	}
+	else if(y + dy > canvas.height-ballRadius){
+		alert("GAME OVER");
+		document.location.reload();
 	}
 	
-   
-  
+	//	if(((y+dy > canvas.height-paddleHeight) && ((x > paddleX) && x < (paddleX + paddleWidth)) )|| (y+dy < 0))
+	if(rightPressed && paddleX <canvas.width-paddleWidth){
+		paddleX += 7;
+	}
+	
+	else if(leftPressed && paddleX > 0){
+		paddleX -= 7;
+	}
+		
 }
 
+function keyDownHandler(e){
+	if(e.keyCode == 39){
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37){
+		leftPressed = true;
+	}
+	
+}
+function keyUpHandler(e){
+	if(e.keyCode == 39){
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37){
+	leftPressed = false;
+	}
+	
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);	
+	
 setInterval(draw, 10);
