0fa18152b9a1e49084e46ec87052de0050b84460
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-03-07 14:42:57 +0000

    Make the bricks disappear

--- js/breakout.js
+++ js/breakout.js
@@ -44,15 +44,17 @@ for(c = 0; c < brickColumnCount; c++){
 function drawBricks(){
 	for( c = 0; c < brickColumnCount; c++){
 		for(r = 0; r < brickRowCount; r++){
-			var brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
-			var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = "0095DD";
-			ctx.fill();
-			ctx.closePath();
+			if(bricks[c][r].status == 1){
+				var brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
+				var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 }
@@ -142,8 +144,11 @@ function collisonDetection(){
 	for(c = 0; c < brickColumnCount; c++){
 		for(r = 0; r < brickRowCount; r++){
 			var b = bricks[c][r];
-			if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+			if(b.status == 1){
+				if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
 				dy = -dy;
+				b.status = 0;
+				}
 			}
 		}
 	}
